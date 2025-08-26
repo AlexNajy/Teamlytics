8321e2aa73aaac32019ea2509b94729c9be4b8ab
lukeduncan <luke.k.duncan@gmail.com>
2025-08-26 23:55:55 +0100

    backend for tasks

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,22 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from api.v1.tasks import tasks_router
 
+load_dotenv()
+frontend_port = os.getenv("FRONTEND_PORT")
 app = FastAPI(title="My FastAPI App", version="1.0.0")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[f"http://localhost:{frontend_port}"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(tasks_router, prefix="/api/v1")
 
 @app.get("/")
