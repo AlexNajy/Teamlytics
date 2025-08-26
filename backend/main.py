656562ee13cf8ade800acfe1ed534640df21235f
lukeduncan <luke.k.duncan@gmail.com>
2025-08-26 22:48:10 +0100

    Backend restructure + run configs + get issues backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
+from api.v1.issues import issues_router
+
 app = FastAPI(title="My FastAPI App", version="1.0.0")
+
+app.include_router(issues_router, prefix="/api/v1")
 
 @app.get("/")
 async def root():
@@ -8,4 +12,4 @@ async def root():
 
 @app.get("/health")
 async def health():
-    return {"status": "This whole p⁄roject has been flagged for plagiarism. Kidding! It's healthy!"}
+    return {"status": "This whole project has been flagged for plagiarism. Kidding! It's healthy!"}
