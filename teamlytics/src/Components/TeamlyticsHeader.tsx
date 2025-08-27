b1da5818712b92321a042e44174e87b9779ad851
anajy <anajy@student.ubc.ca>
2025-08-26 18:45:50 -0700

    Updated header to use themes

--- teamlytics/src/Components/TeamlyticsHeader.tsx
+++ teamlytics/src/Components/TeamlyticsHeader.tsx
@@ -12,7 +12,7 @@ import {
 const useClasses = makeStyles({
     header: {
         width: "100%",
-        //backgroundImage: "linear-gradient(to right, #a958f5, #3f75eb)",
+        backgroundColor: "var(--card)",
         paddingLeft: "1.5rem",
         paddingRight: "1.5rem",
         paddingTop: "1rem",
@@ -31,23 +31,24 @@ const useClasses = makeStyles({
     title: {
         fontSize: "1.25rem",
         fontWeight: "bold",
-        backgroundImage: "linear-gradient(to right, #111827, #111827)",
+        color:"transparent",
+        backgroundImage: 'linear-gradient(135deg, var(--primary), var(--secondary))',
         WebkitBackgroundClip: "text",
         WebkitTextFillColor: "transparent",
     },
     sidebarTrigger: {
         color: "black",
-        padding: "0.5rem", // p-2
-        borderRadius: "0.375rem", // rounded-md
+        padding: "0.5rem",
+        borderRadius: "0.375rem",
         ":hover": {
-            backgroundColor: "lightgray",
+            backgroundColor: "var(--muted)",
         },
     },
     breadcrumbSeparator: {
-        color: "gray",
+        color: "var(--muted-foreground)",
     },
     breadcrumbText: {
-        color: "gray",
+        color: "var(--muted-foreground)",
         fontSize: "1.125rem",
     }
 });
